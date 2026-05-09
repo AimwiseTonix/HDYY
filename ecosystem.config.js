193a5c826743229cdc3864ee597dc455da769632
ToniX <aimwisetonix@gmail.com>
2026-05-10 00:52:25 +0800

    add CDN_BASE to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
     env: {
       PORT: 3000,
       NODE_ENV: 'production',
+      CDN_BASE: 'https://dizf9ndj1sy0a.cloudfront.net',
     },
     max_memory_restart: '500M',
     log_date_format: 'YYYY-MM-DD HH:mm:ss',
